c4bb3bbcb54d1b555138487f69ba19a25e4e444b
Steven DeVere <sdevere@xogrp.com>
2019-01-09 20:58:46 -0500

    Sorts properties of nested objects

--- src/commands/sortProperties.ts
+++ src/commands/sortProperties.ts
@@ -1,35 +1,15 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 let sortProperties = vscode.commands.registerCommand('extension.sortProperties', () => {
-  const { window } = vscode;
-  const documentData = window.activeTextEditor.document;
+  const { window, workspace, WorkspaceEdit } = vscode
+  const documentData = window.activeTextEditor.document
 
-  const extensionIdx = documentData.fileName.lastIndexOf(".");
-  const extension = documentData.fileName.slice(extensionIdx + 1);
+  const styleObj = createNestedStyleObject(documentData)
 
-  if (isStyleExtension(extension)) {
-    let currentObj = {};
-    const currentStack = [];
-    currentStack[0] = currentObj;
+  let edit = new WorkspaceEdit()
+  edit = someFunc(styleObj, documentData.fileName, 0, 0, edit)
 
-    for (let i = 0; i < documentData.lineCount; i++) {
-      const line = documentData.lineAt(i).text;
-      if (line.includes('{')) {
-        currentStack[0][line] = {};
-        currentStack.unshift(currentStack[0][line]);
-        currentObj = currentStack[0];
-      }
-      else if (line.includes('}')) {
-        currentStack.shift();
-      }
-      else {
-        currentObj[line] = ''
-      }
-    }
-
-    console.log('currentStack[0] is', currentStack[0]);
-    console.log('topicGroup is', currentStack[0]['.topicGroup {:'])
-  }
+  workspace.applyEdit(edit)
 
   // const { workspace, WorkspaceEdit } = vscode;
   // const edit = new WorkspaceEdit();
@@ -68,6 +48,80 @@ const isStyleExtension = (extension: string) => {
 		return true
 	}
 	return false
+}
+
+const createNestedStyleObject = (documentData: Object) => {
+
+  const extensionIdx = documentData.fileName.lastIndexOf(".")
+  const extension = documentData.fileName.slice(extensionIdx + 1)
+  const currentStack = []
+
+  if (isStyleExtension(extension)) {
+    let currentObj = {}
+    currentStack[0] = currentObj
+
+    for (let i = 0; i < documentData.lineCount; i++) {
+      const line = documentData.lineAt(i).text
+      if (line.includes('{')) {
+        currentStack[0][line] = {}
+        currentStack.unshift(currentStack[0][line])
+        currentObj = currentStack[0]
+      }
+      else if (line.includes('}')) {
+        currentStack.shift()
+      }
+      else {
+        currentObj[line] = ''
+      }
+    }
+  }
+
+  return currentStack[0]
+}
+
+const someFunc = (styleObj, fileName, startLine, indent, edit) => {
+
+  const { Position, Range, Uri } = vscode
+  const uri = Uri.file(fileName)
+
+  const stringArr = []
+  const objArr = []
+
+  for (const key in styleObj) {
+    if (key && typeof styleObj[key] === 'string') {
+      stringArr.push(key)
+    } else if (typeof styleObj[key] === 'object') {
+      objArr.push(key)
+    }
+  }
+
+  if (stringArr.length) {
+    stringArr.sort()
+    stringArr.forEach((line, idx) => {
+      const start = new Position(+startLine, indent)
+      const end = new Position(+startLine, 140)
+      edit.replace(uri, new Range(start, end), line)
+      console.log(startLine, ' - ', line)
+      startLine++
+    })
+    const p1 = new Position(+startLine, indent)
+    const p2 = new Position(+startLine, 140)
+    edit.replace(uri, new Range(p1, p2), '')
+    startLine++
+  }
+
+  objArr.forEach(objKey => {
+    const start = new Position(+startLine, indent)
+    const end = new Position(+startLine, 140)
+    edit.replace(uri, new Range(start, end), objKey)
+    console.log(startLine, ' - ', objKey)
+    someFunc(styleObj[objKey], fileName, startLine + 1, 0, edit)
+  })
+  // startLine++
+  // const p3 = new Position(+startLine, indent)
+  // const p4 = new Position(+startLine, 140)
+  // edit.replace(uri, new Range(p3, p4), '}')
+  return edit
 }
 
 export default sortProperties
